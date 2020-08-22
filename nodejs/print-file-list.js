9bf301eb514c0643c324c0a01aeb6ae6ed5b7189
clarXo <imp_rove@naver.com>
2020-08-22 23:53:28 +0900

    200822 Node.js - Print File List from Directory(Code Simplify)

--- nodejs/print-file-list.js
+++ nodejs/print-file-list.js
@@ -1,25 +1,29 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
- 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-    if(pathname === '/'){
-      if(queryData.id === undefined){
- 
-        fs.readdir('./data', function(error, filelist){
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-          var list = '<ul>';
-          var i = 0;
-          while(i < filelist.length){
-            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i = i + 1;
-          }
-          list = list+'</ul>';
-          var template = `
+
+var app = http.createServer(function (request, response) {
+
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
+  var title = 'Welcome';
+
+  if (pathname === '/') {
+    fs.readdir('./data', function (error, filelist) {
+      fs.readFile(`${'data/' + queryData.id + '.txt'}`, 'utf8', function (err, description) {
+        if (queryData.id === undefined) {
+          title = 'Welcome';
+          description = 'Hello, Node.js';
+        } else {
+          title = queryData.id;
+        }
+        var list = '<ul>'
+        for(var i = 0; i<filelist.length;i++){
+          list += `<li><a href="?id=${filelist[i].slice(0,-4)}">${filelist[i].slice(0,-4)}</a></li>`
+        }
+        list += '</ul>';
+        var template = `
           <!doctype html>
           <html>
           <head>
@@ -28,54 +32,19 @@ var app = http.createServer(function(request,response){
           </head>
           <body>
             <h1><a href="/">WEB</a></h1>
-            ${list}
+              ${list}
             <h2>${title}</h2>
             <p>${description}</p>
           </body>
           </html>
           `;
-          response.writeHead(200);
-          response.end(template);
-        })
- 
- 
- 
-      } else {
-        fs.readdir('./data', function(error, filelist){
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-          var list = '<ul>';
-          var i = 0;
-          while(i < filelist.length){
-            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i = i + 1;
-          }
-          list = list+'</ul>';
-          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-            var title = queryData.id;
-            var template = `
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              ${list}
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-            </html>
-            `;
-            response.writeHead(200);
-            response.end(template);
-          });
-        });
-      }
-    } else {
-      response.writeHead(404);
-      response.end('Not found');
-    }
-});
+        response.writeHead(200);
+        response.end(template);
+      });
+    });
+  } else {
+    response.writeHead(404);
+    response.end('Not found');
+  }
+})
 app.listen(3000);
