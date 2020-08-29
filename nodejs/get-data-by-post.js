dc1d42cf6694fde95d569602a489d1495a6aba16
clarXo <imp_rove@naver.com>
2020-08-29 23:48:24 +0900

    200825 Node.js - Get Data by Post

--- nodejs/get-data-by-post.js
+++ nodejs/get-data-by-post.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 var app = http.createServer(function (request, response) {
     var _url = request.url;
@@ -47,6 +48,28 @@ var app = http.createServer(function (request, response) {
           response.writeHead(200);
           response.end(template);
         });
+    }else if (pathname === '/create_process'){
+        var body = ''; 
+        request.on('data', function (data) {
+            body += data;
+
+            // Too much POST data, kill the connection!
+            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+            if (body.length > 1e6)
+                request.connection.destroy();
+        });
+
+        request.on('end', function () {
+            var post = qs.parse(body);
+            var title = post.title;
+            var description = post.description;
+            console.log(post);
+            console.log(post.title);
+            console.log(post.description);
+        });
+
+        response.writeHead(200);
+        response.end('SUCCESS');
     } else {
         response.writeHead(404);
         response.end('Not found');
@@ -83,3 +106,32 @@ function templateList(filelist) {
     list = list + '</ul>';
     return list;
 }
+
+/* 
+
+    request.on
+
+        웹브라우저가 post 방식으로 데이터를 전송하고자 할 때,
+            데이터 수신 시 마다 콜백 함수 호출하도록 약속되어 있다.
+            이때, 콜백 함수의 인자 'data'를 통해 정보를 전달
+
+            => 한 번에 다량의 데이터가 전송되어 지연되는 것을 방지하는 콜백 작성
+
+
+        인자의 종류
+            
+            request.on('data'
+
+                데이터 수신 시 콜백
+
+            request.on('end'
+
+                받는 데이터가 없을 시 콜백(정보 수신 없을 때)
+
+
+
+    require('querystring')
+
+        nodejs가 가진 쿼리스트링이라는 모듈 호출
+
+*/
