eea8c366cafa5d906540596bea332eae5e005aaf
clarXo <imp_rove@naver.com>
2020-09-08 23:51:19 +0900

    200908 - Basic JavaScript : Module

---
+++ nodejs/Module/muse.js
@@ -0,0 +1,29 @@
+/* 
+    var M = {
+    v:'v',
+    f:function(){
+        console.log(this.v);
+    }
+}
+*/
+
+var part = require('./mpart.js');
+console.log(part);
+part.f();
+
+
+/* 
+
+    데이터의 정리
+
+        변수
+
+        객체
+
+        모듈
+
+            module.export
+
+            require(경로명)
+
+*/
